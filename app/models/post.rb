ab6198c40f45213e6ce73770d0a48238f21ed30d
lekegitrepo <lekedevrepo@gmail.com>
2019-12-02 15:23:34 -0600

    remove the default scope from post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comment
   has_many :like
 
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
 
   validates :content, presence: true, length: { minimum: 6, maximum: 250 }
 end
