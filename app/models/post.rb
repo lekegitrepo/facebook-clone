e99c61e32ede1bbc087c2e688656bcd10178b82a
lekegitrepo <lekedevrepo@gmail.com>
2019-12-02 12:04:29 -0600

    add validation to post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
   has_many :like
+
+  default_scope -> { order(created_at: :desc) }
+
+  validates :content, presence: true, length: { minimum: 6, maximum: 250 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :post
+  has_many :posts
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
