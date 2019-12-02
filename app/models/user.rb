a53cec9cd568cc7e1329376100bcfc922e666d14
lekegitrepo <lekedevrepo@gmail.com>
2019-12-02 12:28:00 -0600

    add username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :username, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
