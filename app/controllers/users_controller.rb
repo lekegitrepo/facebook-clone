557807b2105fc4c9e4267ead4a0ff9e379ccd981
lekegitrepo <lekedevrepo@gmail.com>
2019-12-03 11:35:09 -0600

    add authentication. Modify user profile page and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'user_profile', to: 'users#show'
   devise_for :users
   root 'pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
