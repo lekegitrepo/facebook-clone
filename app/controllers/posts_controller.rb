6855ef1348997929d03f1d90d53960bd369e633b
lekegitrepo <lekedevrepo@gmail.com>
2019-12-04 10:28:13 -0600

    add action methods to post controller and update route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,33 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order('created_at DESC')
   end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post created successful!'
+    else
+      flash[:danger] = 'Unable to create your post'
+    end
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   # For details on the DSL available within this file,
   # see https://guides.rubyonrails.org/routing.html
+  resources :posts
   resources :users
 
   devise_scope :user do
