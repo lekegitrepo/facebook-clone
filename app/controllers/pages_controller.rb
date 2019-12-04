8dcc6418a77917abd2b7dd6005ab7eeb11a68e8c
lekegitrepo <lekedevrepo@gmail.com>
2019-12-04 08:16:39 -0600

    add show to pages controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def show
+    @user_profile = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get 'user_profile', to: 'users#show'
   devise_for :users
+  get 'user_profile', to: 'pages#show'
   root 'pages#index'
   # For details on the DSL available within this file,
   # see https://guides.rubyonrails.org/routing.html
