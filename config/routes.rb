7a7ebb9bfd3947eb4f031696608de91cf520d140
lekegitrepo <lekedevrepo@gmail.com>
2019-12-04 08:41:56 -0600

    set login and sign up links on the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file,
   # see https://guides.rubyonrails.org/routing.html
   resources :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
 end
