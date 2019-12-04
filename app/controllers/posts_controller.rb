136f1bb299dbb4c2db35d50f7be03a48eb993c33
lekegitrepo <lekedevrepo@gmail.com>
2019-12-04 10:48:46 -0600

    add before action callback to edit and destroy to check existence of a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :check_post, only: %i[edit destroy]
+
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -19,17 +21,29 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = 'Post successful deleted'
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def check_post
+    @post = Post.find(params[:id])
+    return unless @post.nil?
+
+    flash[:danger] = 'Post does not exist'
+    redirect_to root_path
   end
 end
