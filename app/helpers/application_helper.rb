ebade6797c506bc7ed8473cd26ebd141b58194d6
lekegitrepo <lekedevrepo@gmail.com>
2019-12-04 14:51:18 -0600

    create correct user in application helper module for future use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def current_user?(user)
     user == current_user
   end
+
+  private
+
+  # confirms the correct user.
+  def correct_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
