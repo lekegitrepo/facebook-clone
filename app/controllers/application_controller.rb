f7362d0c5fa57791b44151e17429aa4da56d7258
lekegitrepo <lekedevrepo@gmail.com>
2019-12-05 04:14:48 -0600

    remove some unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :check_logged_in_user
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:first_name, :last_name, :username])
-  end
-
-  def check_logged_in_user
-    redirect_to new_user_registration_path unless user_signed_in?
   end
 end
