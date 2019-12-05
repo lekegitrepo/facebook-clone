2ec8e8f005c3ef9415dfd3467385874cce0735bc
lekegitrepo <lekedevrepo@gmail.com>
2019-12-05 07:39:23 -0600

    user test model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe',
+                     email: 'john@gmail.com', password: 'johndoe')
+  }
+
+  before { subject.save }
+
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'should be valid' do
+    subject.password = ' '
+    expect(subject).to_not be_valid
+  end
+
+  it 'should be valid' do
+    subject.last_name = ' '
+    expect(subject).to_not be_valid
+  end
 end
