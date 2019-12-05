08068e812eb76b7751e45e279cea20a7a7cbb498
lekegitrepo <lekedevrepo@gmail.com>
2019-12-05 11:30:45 -0600

    add valid test for creating a post

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -9,26 +9,18 @@ RSpec.describe 'create post', type: :feature do
                     password_confirmation: 'johndoe')
     user.save
     visit '/users/sign_in'
-    fill_in 'user[email]', with: @user.email
-    fill_in 'user[password]', with: @user.password
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_button 'Log in'
   end
 
-  # scenario 'valid user sign up' do
-  #   visit '/users/sign_up'
-  #   fill_in 'First name', with: 'John'
-  #   fill_in 'Last name', with: 'Doe'
-  #   fill_in 'Username', with: 'johndoe'
-  #   fill_in 'Email', with: 'john@gmail.com'
-  #   fill_in 'Password', with: 'johndoe'
-  #   fill_in 'Password confirmation', with: 'johndoe'
-  #   click_button 'Sign up'
+  scenario 'create valid post' do
+    visit root_path
+    fill_in 'post[content]', with: 'New post for test'
+    click_button 'Share'
 
-  #   expect(page).to have_content('johndoe')
-  #   expect(page).to have_content('Applications')
-  #   expect(page).to have_content('Contents and Posts')
-  #   expect(page).to have_content('Advertisements')
+    expect(page).to have_content('New post for test')
 
-  #   expect(page).to have_button('Share')
-  # end
+    expect(page).to have_button('Share')
+  end
 end
