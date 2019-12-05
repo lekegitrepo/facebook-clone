9fc249a791ba2a8316a8ed915ed807dbfb15f52e
lekegitrepo <lekedevrepo@gmail.com>
2019-12-05 08:55:38 -0600

    add association test between user and post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  post = nil
+  subject do
+    User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe',
+             email: 'john@gmail.com', password: 'johndoe')
+  end
+
+  before { subject.save }
+
+  before { post = subject.posts.create(content: 'This is a test post') }
+
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'post with empty content should not be valid' do
+    post.content = ' '
+    expect(post).to_not be_valid
+  end
+
+  it 'create post without user should not be valid' do
+    post = Post.new
+    expect(post).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it 'post belongs to user' do
+      assoc = described_class.reflect_on_association(:user)
+      expect(assoc.macro).to eq :belongs_to
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe',
-                     email: 'john@gmail.com', password: 'johndoe')
-  }
+  subject do
+    User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe',
+             email: 'john@gmail.com', password: 'johndoe')
+  end
 
   before { subject.save }
 
@@ -11,18 +12,25 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'should be valid' do
+  it 'user with empty password should not be valid' do
     subject.password = ' '
     expect(subject).to_not be_valid
   end
 
-  it 'should be valid' do
+  it 'user with empty last name should not be valid' do
     subject.last_name = ' '
     expect(subject).to_not be_valid
   end
 
-  it 'should be valid' do
+  it 'user with email set to nil should not be valid' do
     subject.email = nil
     expect(subject).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it 'user has many posts' do
+      assoc = described_class.reflect_on_association(:posts)
+      expect(assoc.macro).to eq :has_many
+    end
   end
 end
