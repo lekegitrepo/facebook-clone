35387f2d5455120e6b9ef4d2777a2dd7b19474ec
lekegitrepo <lekedevrepo@gmail.com>
2019-12-09 15:16:08 -0600

    add actions to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,25 @@
 class LikesController < ApplicationController
+  before_action :check_post
+
+  def create
+    @like = @post.likes.build(user: current_user)
+    @like.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.delete
+    redirect_to root_path
+  end
 
   private
 
   def check_post
-    @post = Post.find(params[:post_id])
+    @post = Post.find(params[:id])
+    return unless @post.nil?
+
+    flash[:danger] = 'Post does not exist'
+    redirect_to root_path
   end
 end
