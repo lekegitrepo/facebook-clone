0d37c847009947d62bcbfc76feda4e23473f2d96
lekegitrepo <lekedevrepo@gmail.com>
2019-12-09 16:34:12 -0600

    add like functionality

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,10 @@
 module LikesHelper
+  def liked?(post)
+    # Like.where(post_id: post.id, user_id: current_user.id).exists?
+    Like.where(post_id: post.id, user_id: current_user.id).present?
+  end
+
+  def like_post(post)
+    Like.where(post_id: post.id, user_id: current_user.id).first
+  end
 end
