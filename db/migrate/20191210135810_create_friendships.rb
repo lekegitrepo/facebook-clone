34fb08902576642c56fb35df0222cc1aedb620df
lekegitrepo <lekedevrepo@gmail.com>
2019-12-10 09:29:30 -0600

    db migration for friendship

--- db/migrate/20191210135810_create_friendships.rb
+++ db/migrate/20191210135810_create_friendships.rb
@@ -1,11 +1,12 @@
 class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
-      t.boolean :confirmed
+      t.references :user, index: true, foreign_key: true
+      t.references :friend, index: true
+      t.boolean :confirmed, default: false
 
       t.timestamps
     end
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
