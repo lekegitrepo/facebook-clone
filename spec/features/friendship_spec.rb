22f252cac142df01b22be4ed0499ae62195a6c30
lekegitrepo <lekedevrepo@gmail.com>
2019-12-13 16:15:45 -0600

    create feature test for friendship

---
+++ spec/features/friendship_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :feature do
+  before :each do
+    @first_user = User.new(first_name: 'John', last_name: 'Doe', username: 'johndoe',
+                           email: 'john@gmail.com', password: 'johndoe',
+                           password_confirmation: 'johndoe')
+    @first_user.save
+    @second_user = User.new(first_name: 'Jane', last_name: 'Doe', username: 'janedoe',
+                            email: 'jane@gmail.com', password: 'janedoe',
+                            password_confirmation: 'janedoe')
+    @second_user.save
+
+    visit '/users/sign_in'
+    fill_in 'Email', with: @first_user.email
+    fill_in 'Password', with: @first_user.password
+    click_button 'Log in'
+    visit users_path
+    click_button 'Add new friend'
+    click_on 'Logout'
+  end
+
+  scenario 'successfully' do
+    fill_in 'Email', with: @second_user.email
+    fill_in 'Password', with: @second_user.password
+    click_button('Log in')
+    visit users_path
+    click_button 'Accept'
+    expect(page).to have_content("#{@first_user.username} is one of my friend")
+  end
+end
