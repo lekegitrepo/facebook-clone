a6e6ffd3e32067192f3b711799aa5d44ad5391d7
lekegitrepo <lekedevrepo@gmail.com>
2019-12-14 14:21:52 -0600

    reset db

--- db/migrate/20191214200859_add_indices_to_friendship.rb
+++
@@ -1,7 +0,0 @@
-class AddIndicesToFriendship < ActiveRecord::Migration[6.0]
-	def change
-		add_index :friendships, :user_id
-		add_index :friendships, :friend_id
-		add_index :friendships, [:user_id, :friend_id], unique: true
-	end
-end
