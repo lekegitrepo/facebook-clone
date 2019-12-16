f5992736e1d265a33a802356c3eaaeec3ecbee7a
lekegitrepo <lekedevrepo@gmail.com>
2019-12-16 15:59:05 -0600

    add the final step of the authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
-  config.omniauth :facebook, "<%= ENV['FACEBOOK_APP_ID'] %>", "<%= ENV['FACEBOOK_APP_SECRET'] %>", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "503157103878828", "b2a40006161ea754f95efa3c4aad8ffd", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
