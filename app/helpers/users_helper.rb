ce9dfed82b8f94540aee16dfddb6f32e616e86d7
lekegitrepo <lekedevrepo@gmail.com>
2019-12-16 04:34:50 -0600

    move logic from _user_status partial to userhelper class

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,4 +15,16 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: 'gravatar')
   end
+
+  def user_status(user)
+    if current_user.friend?(user)
+      "#{user.username} is one of my friend"
+      button_to 'Unfriend', user_reject_request_path(user), method: :delete, class: 'btn btn-danger mb-2'
+    elsif current_user.pending_friends.include?(user)
+      button_to 'Cancel request', user_reject_request_path(user), method: :delete, class: 'btn btn-danger mb-2'
+    elsif current_user.friend_requests.include?(user)
+    else
+      button_to 'Add new friend', user_friendships_path(user), class: 'btn btn-info mb-2'
+    end
+  end
 end
