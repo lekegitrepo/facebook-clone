97e21bb841cb422f7c8cc4ef64f0c936005204d2
lekegitrepo <lekedevrepo@gmail.com>
2019-12-16 15:25:22 -0600

    add omniauth onclick login to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,4 +60,21 @@ class User < ApplicationRecord
     friend_ids = friends.map(&:id)
     Post.where('user_id IN (?) OR user_id=?', friend_ids, id)
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
