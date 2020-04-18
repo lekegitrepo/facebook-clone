35950f2468df0261838fd247e764ac07b8c3b458
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-04-18 15:47:36 -0600

    load bootstrap through BootstrapCDN

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "bootstrap";
+//import "bootstrap";
 //import "./src/application.scss";
 
 require("@rails/ujs").start();
