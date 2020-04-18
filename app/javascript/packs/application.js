e4ed400870e674a412f5b31083ea9e61646adb0f
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-04-18 14:49:28 -0600

    import bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,13 @@
 // that code so it'll be compiled.
 
 import "bootstrap";
-import "./src/application.scss";
+//import "./src/application.scss";
 
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require('jquery')
+require("jquery");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
